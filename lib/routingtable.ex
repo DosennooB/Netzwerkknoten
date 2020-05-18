2696c2fec922949f33d527c73571573819c961df
ploew <patrick.loew@smail.th-koeln.de>
2020-05-19 01:13:25 +0200

    Routing neue message funktionen

--- lib/routingtable.ex
+++ lib/routingtable.ex
@@ -9,7 +9,7 @@ defmodule Routingtable do
             send con_pid, {:get_routingtable, self()}
 
             receive do
-              {:set_routingtable, table = []} ->
+              {:set_routingtable, table} ->
                 routingtable(link_pid, con_pid, table)
             end
         end
@@ -20,6 +20,21 @@ defmodule Routingtable do
     receive do
       {:rout_get_routingtable, pid} ->
         send pid, {:rout_routingtable, table}
+        routingtable(link_pid, con_pid, table)
+
+      {:rout_set_routingtable, table_new} ->
+        routingtable(link_pid, con_pid, table_new)
+
+      {:rout_message, m = %Message{}} ->
+        hop = Keyword.get_values(table, m.receiver)
+        send link_pid, {:sending, hop, m}
+
+      {:link_verteiler_pid, link_pid_new} ->
+        routingtable(link_pid_new, con_pid, table)
+
+      {:conection_pid, con_pid_new} ->
+        routingtable(link_pid, con_pid_new, table)
+#TODO
     end
 
   end
