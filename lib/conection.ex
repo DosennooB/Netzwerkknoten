9313cfc27714c272d29d7e6f872038a1628e9edb
ploew <patrick.loew@smail.th-koeln.de>
2020-06-28 23:03:24 +0200

    Label hausgewurfen

--- lib/conection.ex
+++ lib/conection.ex
@@ -1,6 +1,3 @@
-
-
-
 defmodule Conection do
 @moduledoc """
 Verwaltet die Graphen um die Routingtablle zu be
@@ -15,7 +12,7 @@ Verwaltet die Graphen um die Routingtablle zu be
           {:routingtable_pid, routingtable_pid} ->
             g = Graph.new()
             |> Graph.add_vertex(router_pid)
-            |> Graph.add_edge(router_pid, router_pid, label: 1, weight: 1)
+            |> Graph.add_edge(router_pid, router_pid, weight: 1)
             send routingtable_pid, {:rout_set_routingtable, self(), %{router_pid => router_pid}, %{router_pid => 1}}
             conection(g, router_pid, routingtable_pid)
         end
@@ -27,26 +24,29 @@ Verwaltet die Graphen um die Routingtablle zu be
       {:con_add_link, m = %Message{}} ->
         ng = newlink(g , m.data, routingtable_pid)
         _rg = graphReduzieren(ng, router_pid)
-        #Routingtable erstellen
+        #TODO Routingtable erstellen
+        #TODO Hoptable erstellen
       {:con_remove_link, m = %Message{}} ->
         true
+        #TODO
       {:con_remove_router, value} ->
         true
+        #TODO
     end
   end
 
   @doc """
   fügt dem Graph neue Kanten hinzu und updated die Kanten wenn sie alt sind.
-  Wenn eine Kante erstellt wird wird ein Broadcast gesendet.
+  Wenn eine Kante erstellt wird oder verändert wird, wird ein Broadcast gesendet.
   """
   defp newlink(g = %Graph{}, [e=%Graph.Edge{} | t],routingtable_pid) do
     edgebroadcast = %Message{receiver: routingtable_pid, sender: routingtable_pid, type: :new_link, data: e, size: 4}
     case Graph.edges(g, e.v1, e.v2) do
       [h = %Graph.Edge{}|_t] ->
         cond do
-          e.label < h.label ->
+          e.weight != h.weight ->
             send routingtable_pid, {:rout_broadcast, edgebroadcast}
-            Graph.update_labelled_edge(g, h.v1, h.v2, h.label, weight: e.weight,label: e.label)
+            Graph.update_edge(g, h.v1, h.v2,  weight: e.weight)
             |> newlink( t, routingtable_pid)
           true ->
             newlink(g, t, routingtable_pid)
